fc1d0a91c7f0d16a57b24c357d547a73af70d20d
Антон <anton_kopnin@mail.ru>
2024-02-02 14:24:36 +0300

    new version 7

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,10 +8,6 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='token')
-
-DEBUG = bool(os.getenv('DEBUG', 'False').lower())
-
 DEBUG = os.getenv('DEBUG', False) == True
 
 ALLOWED_HOSTS = []
